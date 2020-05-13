44c8f7d29ec405ee59febb86c41319b148407f81
Mikita Saladukha <mikita.saladukha@gmail.com>
2020-05-13 14:42:25 +0300

    creation sping-boot and thymeleaf

---
+++ demo2/src/main/java/com/packtpublishing/tddjava/ch09/FibonacciFeatureConfig.java
@@ -0,0 +1,18 @@
+package com.packtpublishing.tddjava.ch09;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "features.fibonacci")
+public class FibonacciFeatureConfig {
+    private boolean restEnabled;
+    public boolean isRestEnabled() {
+        return restEnabled;
+    }
+    public void setRestEnabled(boolean restEnabled) {
+        this.restEnabled = restEnabled;
+    }
+}
---
+++ demo2/src/main/java/com/packtpublishing/tddjava/ch09/FibonacciService.java
@@ -0,0 +1,10 @@
+package com.packtpublishing.tddjava.ch09;
+
+import org.springframework.stereotype.Service;
+
+@Service("fibonacci")
+public class FibonacciService {
+    public int getNthNumber(int n) {
+        return -1;
+    }
+}
